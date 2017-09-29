8a70a213187923925c27733435da8a80b2c5eee9
Emil Tsokov <emil@athlonsofia.com>
2017-09-29 13:34:10 +0300

    videolist-videodetail functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import SearchBar from './components/search_bar'
 const API_KEY = 'AIzaSyDW4kRvYg7UOKARCDzyKABvLGfIcd6KBPg';
 
-//create new comp
-const App = function() {
-  return (
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      videos: [],
+      selectedVideo: null
+    }
+
+    YTSearch({ key: API_KEY , term : 'стругаре'}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    })
+  }
+  render () {
+    return (
     <div>
       <SearchBar />
+      <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
     </div>
   )
+  }
 }
-
-//insert it into DOM
 
 ReactDOM.render(<App />, document.querySelector('.container'));
